54782c75554de8f2350524bb75be4c99fa065e34
andrea-w-wang <ayw255@nyu.edu>
2022-12-22 17:26:30 +0800

    feat: split train/test/dev

--- prep_data.py
+++ prep_data.py
@@ -17,7 +17,10 @@ for lang in data['language'].unique():
     lang_data = data[data['language'] == lang].copy()
     lang_data['binary_labels'] = lang_data['label'].apply(lambda l: 0 if l<=3 else 1)
     lang_data['original_labels'] = lang_data['label']
-    lang_data = lang_data[['text', 'binary_labels', 'original_labels']].to_dict()
-    output_filename = f"clean_{lang_code_map[lang]}.pk"
-    pk.dump(lang_data, open(data_dir + output_filename, "wb"))
-    
+    lang_data = lang_data[['text', 'binary_labels', 'original_labels']]
+    lang_train, lang_test = train_test_split(lang_data, test_size=0.2)
+    lang_test, lang_dev = train_test_split(lang_test, test_size=0.5)
+    print(len(lang_train), len(lang_dev), len(lang_test))
+    pk.dump(lang_train.to_dict(), open(data_dir + f"clean_{lang_code_map[lang]}_train.pk", "wb"))
+    pk.dump(lang_test.to_dict(), open(data_dir + f"clean_{lang_code_map[lang]}_test.pk", "wb"))
+    pk.dump(lang_dev.to_dict(), open(data_dir + f"clean_{lang_code_map[lang]}_dev.pk", "wb"))
